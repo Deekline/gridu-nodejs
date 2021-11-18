787397d4f54cfdd4ddeb07557ef28bcc90f728d3
myemets <mykola.yemets@macys.com>
2021-11-18 12:51:14 +0200

    Fixed errors with date. Add validation for limit, transform date to user friendly

--- src/controllers/logs.controller.js
+++ src/controllers/logs.controller.js
@@ -1,7 +1,7 @@
 const {HttpNotFoundException, HttpBadRequestException} = require('../../error-handling');
 const {getUserExercises, getExerciseByUserId} = require('../services/exercises.service');
 const {getUserById} = require('../services/users.service');
-const {isDateValid} = require('../utils');
+const {isDateValid, transformTimestamp} = require('../utils');
 
 const getLogs = async (req, res) => {
   try {
@@ -14,13 +14,24 @@ const getLogs = async (req, res) => {
 
     const from = req.query.from;
     const to = req.query.to;
+    const limit = req.query.limit
 
     if ((from && !isDateValid(from)) || (to && !isDateValid(to))) {
       throw new HttpBadRequestException('Date format is not valid, use yyyy-mm-dd');
     }
 
-    user.exercises = await getUserExercises(userId, from, to, req.query.limit);
+    if(!Number.isInteger(parseFloat(limit))) {
+      throw new HttpBadRequestException('Limit format is not valid, use integer number')
+    }
+
+    user.exercises = await getUserExercises(userId, from, to, limit);
+
+    if(user.exercises.length) {
+      user.exercises.forEach(transformTimestamp)
+    }
+
     const userExercises = await getExerciseByUserId(userId);
+
     user.count = userExercises.length;
 
     res.json(user);
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,13 +1,23 @@
 function isDateValid(date) {
-  const regexp = new RegExp(/[1-9][0-9]{3}-[0-1][0-9]-[0-9][1-9]/);
+  const regexp = new RegExp(/^([0-9]{4}[-/]?((0[13-9]|1[012])[-/]?(0[1-9]|[12][0-9]|30)|(0[13578]|1[02])[-/]?31|02[-/]?(0[1-9]|1[0-9]|2[0-8]))|([0-9]{2}(([2468][048]|[02468][48])|[13579][26])|([13579][26]|[02468][048]|0[0-9]|1[0-6])00)[-/]?02[-/]?29)$/);
+
   return regexp.test(date);
 }
 
+const _formatDate = (timestamp) => {
+  return new Date(timestamp)
+}
+
 function getTime(time) {
-  return new Date(time).getTime()
+  return _formatDate(time).getTime()
+}
+
+const transformTimestamp = (ex) => {
+    ex.date = _formatDate(ex.date)
 }
 
 module.exports = {
   isDateValid,
-  getTime
+  getTime,
+  transformTimestamp
 };
